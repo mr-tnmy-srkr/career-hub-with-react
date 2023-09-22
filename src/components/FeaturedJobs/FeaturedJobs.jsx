abd2eed17f85e37d8550863333eeaae063d294e5
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-22 20:11:10 +0530

    updated

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,14 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
+import { useNavigation } from "react-router-dom";
+import { BallTriangle } from "react-loader-spinner";
 
 const FeaturedJobs = () => {
+  const [jobs, setJobs] = useState([]);
 
-// const[]
+  const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <div>
-           <h2 className="text-5xl text-center">Featured Jobs</h2> 
-           <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
-        </div>
-    );
+  // const axios = require('axios');
+  useEffect(() => {
+    // const fetchData = () => {
+    axios
+      .get("data/jobs.json")
+      .then((response) => {
+        setJobs(response.data);
+      })
+
+      .finally(() => {
+        setLoading(false);
+      });
+    // }
+    // fetchData();
+  }, []);
+
+  console.log(jobs);
+
+  return (
+    <div>
+      <div>
+        <h2 className="text-5xl text-center">Featured Jobs</h2>
+        <p className="text-center">
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
+      </div>
+      <div>
+       
+      </div>
+      {
+        navigation.state === "loading" ?  <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#4fa94d"
+          ariaLabel="ball-triangle-loading"
+          wrapperClass={{}}
+          wrapperStyle=""
+          visible={true}
+        /> : 
+      
+      <div>
+        {jobs.map((job) => (
+          <Job key={job.id} job={job}></Job>
+        ))}
+      </div>
+      }
+    </div>
+  );
 };
 
 export default FeaturedJobs;
